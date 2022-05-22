790d69488eae647cb8a6a68a07237d5d731d5f5a
karma <nsm4421@google.com>
2022-05-22 18:42:59 +0900

    build

--- src/components/utils/Button.js
+++
@@ -1,9 +0,0 @@
-import { Button as MuiButton } from "@mui/material";
-
-const Button = ({label, onClick})=>{
-    return (
-        <MuiButton onClick={onClick}>{label}</MuiButton>
-    )
-}
-
-export default Button;
